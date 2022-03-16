d6410a2372b5b9795a4e5fab3810b67d41b52b4f
Hyper <rjfance@gmail.com>
2022-03-16 00:22:53 -0400

    Base Files

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Hyper & Obtaizen'
+description 'Sign Robbery Script'
+version '1.0.0'
+
+shared_script 'config.lua'
+server_script 'server/*'
+client_script 'client/*'
+
+lua54 'yes'
---
+++ server/server.lua
@@ -0,0 +1,64 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+
+RegisterNetEvent("qb-signrobbery:server:StopSign", function()
+    local src = source
+    local xPlayer = QBCore.Functions.GetPlayer(tonumber(src))
+
+	xPlayer.Functions.AddItem("stopsign", 1, false)
+	TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['stopsign'], "add")
+end)
+
+RegisterNetEvent("qb-signrobbery:server:WalkingManSign", function()
+    local src = source
+    local xPlayer = QBCore.Functions.GetPlayer(tonumber(src))
+
+	xPlayer.Functions.AddItem("walkingmansign", 1, false)
+	TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['walkingmansign'], "add")
+end)
+
+RegisterNetEvent("qb-signrobbery:server:DontBlockIntersectionSign", function()
+    local src = source
+    local xPlayer = QBCore.Functions.GetPlayer(tonumber(src))
+
+	xPlayer.Functions.AddItem("dontblockintersectionsign", 1, false)
+	TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['dontblockintersectionsign'], "add")
+end)
+
+RegisterNetEvent("qb-signrobbery:server:UTurnSign", function()
+    local src = source
+    local xPlayer = QBCore.Functions.GetPlayer(tonumber(src))
+
+	xPlayer.Functions.AddItem("uturnsign", 1, false)
+	TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items['uturnsign'], "add")
+end)
+
+
+---------------------
+--- Usable Signs ----
+---------------------
+
+
+QBCore.Functions.CreateUseableItem("stopsign", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent('qb-signrobbery:use:StopSign', src, item)
+end)
+
+QBCore.Functions.CreateUseableItem("walkingmansign", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent('qb-signrobbery:use:WalkingManSign', src, item)
+end)
+
+QBCore.Functions.CreateUseableItem("dontblockintersectionsign", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent('qb-signrobbery:use:DontBlockIntersectionSign', src, item)
+end)
+
+QBCore.Functions.CreateUseableItem("uturnsign", function(source, item)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    TriggerClientEvent('qb-signrobbery:use:UturnSign', src, item)
+end)
